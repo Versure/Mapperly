97f4d0905d4b7d4bc08dc688a5c02be5a6a62690
Jop Reuvers <jreuvers@mypoiworld.com>
2024-11-04 16:42:45 +0100

    * Initial commit.

---
+++ Mapperly/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Riok.Mapperly.Abstractions;
+
+public class Car
+{
+    public string Brand { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+}
+
+public class CarDto
+{
+    public string Brand { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
+[Mapper]
+public partial class Mapper {
+    public partial CarDto Map(Car source, string name);
+}
